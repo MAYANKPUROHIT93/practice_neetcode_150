1779f8323cf0f8dfa5539d9d75a9fd2ad5e16a06
MAYANKPUROHIT93 <mayank.prohit@gmail.com>
2024-07-22 01:51:28 +0530

    added solutions

---
+++ practice_neetcode/practice_neetcode/src/test/java/com/practice/practice_neetcode/PracticeNeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.practice_neetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeNeetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
